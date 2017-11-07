5e2bbf50eeccfa93752df4005ae9f34930dc033e
Kerry <kpeay@uncc.edu>
2017-11-07 20:41:45 +0000

    Fixed branch?

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,5 +1,7 @@
+require 'greeter'
+
 # Default is World
-# Author: Jim Weirich (kpeay@uncc.edu)
 name = ARGV.first || "World"
 
-puts "Hello, #{name}!"
+greeter = Greeter.new(name)
+puts greeter.greet
